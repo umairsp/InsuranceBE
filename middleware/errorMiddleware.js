2987543eff68abda904bd2888fdc369385e471c3
umairsp <umair.spdev@gmail.com>
2026-03-09 05:27:48 +0530

    feat: Add error handling middleware for 404 and general server errors.

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -16,7 +16,9 @@ const errorHandler = (err, req, res, next) => {
 
     res.json({
         message: err.message,
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+        stack: err.stack,
+        path: req.originalUrl,
+        method: req.method
     });
 };
 
